e11ec5336757a7d111f148f1c6f10dfd2ec92051
mik <mikail.aziev@gmail.com>
2017-05-18 20:47:59 +0300

    some new changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
+    var elements = {
+        burger: $('.burger'),
+        menu: $('.pages'),
+    };
 
+    $(elements.burger).on('click', function (e) {
+        e.preventDefault();
+
+        $(elements.menu).toggleClass('active');
+    });
 
 });
